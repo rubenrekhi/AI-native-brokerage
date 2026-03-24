2af7fc64fecfe972846ae27ee5e9614b9b501679
Ruben Rekhi <rubenrekhi4@gmail.com>
2026-03-24 18:31:29 -0400

    feat(ios): scaffold Saturn Xcode project
    Add initial Xcode project with app entry point, ContentView, asset
    catalogs, and default unit/UI test targets.
    
    Refs: SEV-105

---
+++ saturn-app/Saturn/Saturn/SaturnApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaturnApp.swift
+//  Saturn
+//
+//  Created by Ruben Rekhi on 2026-03-24.
+//
+
+import SwiftUI
+
+@main
+struct SaturnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
