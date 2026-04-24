90832ce3e9c277ef0dee8a164db1720090d6dfc7
Ruben Rekhi <rubenrekhi4@gmail.com>
2026-04-24 16:37:47 -0400

    chore(config): Make supabase service role key mandatory

--- sevino-api/app/config.py
+++ sevino-api/app/config.py
@@ -45,7 +45,7 @@ class Settings(BaseSettings):
     redis_url: str
     supabase_url: str
     supabase_anon_key: str
-    supabase_service_role_key: str = ""
+    supabase_service_role_key: str
     api_key: str = ""
     alpaca_api_key: str
     alpaca_secret_key: str
